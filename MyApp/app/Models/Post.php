c5dcde84f3c15528c573b04c49cc22edf897b898
Anik Saha <aniksaha200r@gmail.com>
2023-06-20 03:57:13 +0600

    update

---
+++ MyApp/app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    /*  default values
+     *  protected $table = 'posts';
+     *  protected $primaryKey = 'id';
+     * */
+}
--- MyApp/routes/web.php
+++ MyApp/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +19,51 @@ use App\Http\Controllers\PostsController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/post/{id}', [PostsController::class, 'index']);
+
+/*
+|--------------------------------------------------------------------------
+| Raw sql query
+|--------------------------------------------------------------------------
+*/
+//Route::get('/insert', function () {
+//    DB::insert('insert into posts (title, content) values (?, ?)', ["This is manual query", "I am awesome"]);
+//});
+//
+//Route::get("/view", function (){
+//   $response = DB::select("select * from posts where true");
+//   $rendering_content = "";
+//    for ($i = 0; $i < count($response); $i++) {
+//        $rendering_content .= $response[$i]->title." ".$response[$i]->content." ".$i.", ";
+//   }
+//   return $rendering_content;
+//});
+//
+//Route::get("/update", function (){
+//   $updated_value = DB::update('update posts set title ="What\'s up  bro updated" where id=?', [3]);
+//   return $updated_value;
+//});
+//
+//Route::get("/delete", function (){
+//    $deleted = DB::delete('delete from posts where id=?', [3]);
+//    return $deleted;
+//});
+
+/*
+|--------------------------------------------------------------------------
+| ORM
+|--------------------------------------------------------------------------
+*/
+Route::get("/read", function () {
+   $posts = Post::all();
+   foreach ($posts as $post) {
+       return $post->title;
+   }
+});
+
+Route::get("/find", function () {
+    return Post::find(2);
+});
 
 //Route::get('/test', [PostsController::class, 'test']);
 
